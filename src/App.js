53aa8e790702109ba072c994f022456cce9a2cee
samedan <same.dan@gmail.com>
2019-10-14 15:24:56 +0200

    Finoshed Sign in&up

--- src/App.js
+++ src/App.js
@@ -25,21 +25,14 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            },
-            () => {
-              console.log('added user to BDD');
-              console.log(this.state);
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
             }
-          );
+          });
         });
       } else {
-        console.log('user NEW to the DBB');
         this.setState({ currentUser: userAuth });
       }
     });
