65464058fd987062bfda45eed167c589ea571357
samedan <same.dan@gmail.com>
2019-10-15 16:29:20 +0200

    CartIcon & CartDropdown, Crt Reducer

--- src/components/header/headers.component.jsx
+++ src/components/header/headers.component.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './headers.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -27,12 +29,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser: currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 // const store = createStore(
 //   rootReducer /* preloadedState, */,
 //   +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export default store;
