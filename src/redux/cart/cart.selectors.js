d84a6e3a870d93fed17b3b3f1ae5aa26bf899bcf
samedan <same.dan@gmail.com>
2019-10-16 11:15:06 +0200

    Updating with reselect, repmaced cart selectors with memoize selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+// selectCartItems gets only the part of the state interested in
+export const selectCartItemCount = createSelector(
+  // the result is passed as props to cart-icon as 'itemCount'
+
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
